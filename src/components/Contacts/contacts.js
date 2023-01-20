f1982729004bca38b6a095ae1a3f6eb0a9588672
meryemTuzcu <meryem.tuzcu@mega.com.tr>
2023-01-20 15:34:04 +0300

    deneme
    deneme

--- src/components/Contacts/contacts.js
+++ src/components/Contacts/contacts.js
@@ -7,74 +7,74 @@ import ControlTodo from "../ControlTodo/controlTodo";
 
 
 function Contacts() {
-    const[contacts, setContacts] = useState([
-        {new_todo: 'Mehmet'},
-        {new_todo: "Ayşe"},
-        {new_todo: "Leyla"}
-      ]);
-    
-    useEffect(() => {
-        if (localStorage.getItem("contacts") === null) {
-          localStorage.setItem("contacts", JSON.stringify([]))
-        } else {
-            setContacts(JSON.parse(localStorage.getItem("contacts")))
-        }
-      }, [])
-      const [status, setStatus] = useState("");
-      const [filteredContact, setfilteredContact] = useState([]);
-     
-    
-      useEffect(() => {
-        const filterHandler = () => {
-    
-          switch (status) {
-            case "completed":
-              setfilteredContact(contacts.filter((todo) => todo.completed === true));
-              break;
-            case "active":
-              setfilteredContact(contacts.filter((todo) => todo.completed === false));
-              break;
-              
-            default:
-              setfilteredContact(contacts);
-              break;
-          }
-        };
-        saveLocalcontacts()
-        filterHandler();
-        // eslint-disable-next-line
-      }, [contacts, status]);
-    
-      // Local Storage
-      const saveLocalcontacts = () => {
-        localStorage.setItem("contacts", JSON.stringify(contacts));
+  const [contacts, setContacts] = useState([
+    { new_todo: 'Mehmet' },
+    { new_todo: "Ayşe" },
+    { new_todo: "Leyla" }
+  ]);
+
+  useEffect(() => {
+    if (localStorage.getItem("contacts") === null) {
+      localStorage.setItem("contacts", JSON.stringify([]))
+    } else {
+      setContacts(JSON.parse(localStorage.getItem("contacts")))
+    }
+  }, [])
+  const [status, setStatus] = useState("");
+  const [filteredContact, setfilteredContact] = useState([]);
+
+
+  useEffect(() => {
+    const filterHandler = () => {
+
+      switch (status) {
+        case "completed":
+          setfilteredContact(contacts.filter((todo) => todo.completed === true));
+          break;
+        case "active":
+          setfilteredContact(contacts.filter((todo) => todo.completed === false));
+          break;
+
+        default:
+          setfilteredContact(contacts);
+          break;
       }
-    
-    
-    return (
-        <div >
-            <section className="todoapp">
-                <header className="header">
-                    <h1>todos</h1>
-                    <Form addContact={setContacts} contacts={filteredContact}  />
+    };
+    saveLocalcontacts()
+    filterHandler();
+    // eslint-disable-next-line
+  }, [contacts, status]);
 
-                </header>
-
-                
-                <List   setContacts={setContacts} contacts={filteredContact} />
-
-                <ControlTodo contacts={contacts} setContacts={setContacts} status={status} setStatus={setStatus} filteredContact={filteredContact}/>
-            </section>
-
-            <footer className="info">
-                <p>Created by <a target="_blank" href="https://www.linkedin.com/in/berfin-meryem-tuzcu-b97417224/">B. Meryem Tuzcu</a></p>
-            </footer>
+  // Local Storage
+  const saveLocalcontacts = () => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }
 
 
+  return (
+    <div >
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <Form addContact={setContacts} contacts={filteredContact} />
 
-            
-        </div>
-    )
+        </header>
+
+
+        <List setContacts={setContacts} contacts={filteredContact} />
+
+        <ControlTodo contacts={contacts} setContacts={setContacts} status={status} setStatus={setStatus} filteredContact={filteredContact} />
+      </section>
+
+      <footer className="info">
+        <p>Created by <a target="_blank" href="https://www.linkedin.com/in/berfin-meryem-tuzcu-b97417224/">B. Meryem Tuzcu</a></p>
+      </footer>
+
+
+
+
+    </div>
+  )
 }
 
 export default Contacts
